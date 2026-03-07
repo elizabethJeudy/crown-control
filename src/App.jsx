8000ca620afa8d5ecd7524dfa16cd8fecd40508a
elizabethJeudy <ejeudy1@wgu.edu>
2026-03-06 21:38:19 -0500

    created sample cases

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
 const status = ['Incoming', 'In Progress', 'Ready for Delivery', 'Completed']
 const priority = ['Normal', 'Rush', 'Next Day']
 
+// sample cases 
+const cases = [
+  {
+    id: 1,
+    caseNumber: 'CASE-000001',
+    patient: 'Roberts, James',
+    doctor: 'Dr. Smith',
+    type: 'Single crown',
+    material: 'Full Contour Zirconia',
+    teeth: '5',
+    status: 'In Progress',
+    priority: 'Normal',
+    dueDate: '03/20/2026',
+    notes: 'Please fabriciate fcz single crown for tooth #5'
+  }
+]
